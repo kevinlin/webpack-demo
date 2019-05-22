75ce164d4e430b10bc3b535df560192a32445238
Kevin Lin <kevin.lin@zuhlke.com>
2019-05-22 17:42:35 +0800

    Asset Management - https://webpack.js.org/guides/asset-management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
+import Icon from './icon.jpg';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
