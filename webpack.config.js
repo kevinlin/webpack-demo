69b3fdfbe472fe1078d30e6d7259ff69f22aea5a
Kevin Lin <kevin.lin@zuhlke.com>
2019-05-24 18:01:37 +0800

    Development - https://webpack.js.org/guides/development

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,14 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new CleanWebpackPlugin(),
